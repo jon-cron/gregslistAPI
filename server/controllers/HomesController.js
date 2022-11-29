5395657023362f5b032058c6d5264b7b34ce77f6
jon-cron <jonathanacm95@gmail.com>
2022-11-29 14:46:33 -0700

    houses is done

--- server/controllers/HomesController.js
+++ server/controllers/HomesController.js
@@ -1,3 +1,4 @@
+import { dbContext } from "../db/DbContext.js";
 import { homesService } from "../services/HomesService.js";
 import BaseController from "../utils/BaseController.js";
 
@@ -10,18 +11,37 @@ export class HomesController extends BaseController{
     this.router
     .get('', this.getAll)
     .post('', this.create)
+    .delete('/:id', this.remove)
+    .put('/:id', this.update)
   }
   async getAll(req, res, next){
     try {
       const homes = await homesService.getAll()
-      return homes
+      return res.send(homes)
     } catch (error) {
       next(error)
     }
   }
   async create(req, res, next){
     try {
-      
+      const home = homesService.create(req.body)
+      return res.send(home)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async remove(req, res, next){
+    try {
+      const message = await homesService.remove(req.params.id)
+      return res.send(message)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async update(req, res, next){
+    try {
+      const updated = await homesService.update(req.params.id, req.body)
+      return res.send(updated)
     } catch (error) {
       next(error)
     }
--- server/services/HomesService.js
+++ server/services/HomesService.js
@@ -1,4 +1,5 @@
 import { dbContext } from "../db/DbContext.js"
+import { BadRequest } from "@bcwdev/auth0provider/lib/Errors.js"
 
 
 
@@ -8,6 +9,33 @@ class HomesService{
  async getAll(){
   const homes = await dbContext.Homes.find()
   return homes
+  }
+
+  async create(homeData){
+    const newHome = await dbContext.Homes.create(homeData)
+    return newHome
+  }
+  async remove(id){
+    const home = await dbContext.Homes.findById(id)
+    if(!home) throw new BadRequest('no home at this id' + id)
+    await home.remove()
+    return `deleted ${home.year}`
+  }
+
+  async update(id, homeData){
+    const original = await dbContext.Homes.findById(id)
+    if(!original) throw new BadRequest('no home at this id' + id)
+
+    original.bedrooms = homeData.bedrooms ? homeData.bedrooms : original.bedrooms
+    original.bathrooms = homeData.bathrooms ? homeData.bathrooms : original.bathrooms
+    original.price = homeData.price ? homeData.price : original.price
+    original.imgUrl = homeData.imgUrl ? homeData.imgUrl : original.imgUrl
+    original.year = homeData.year ? homeData.year : original.year
+    original.description = homeData.description ? homeData.description : original.description
+
+    await original.save()
+
+  return original
   }
 
 }
